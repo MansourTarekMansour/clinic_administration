9b3cffc29925862c1c22db327a40bf880e1e70de
Mansour Tarek Mansour <mansourtarek100@gmail.com>
2023-04-20 06:29:24 +0200

    Auth register validator

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -17,19 +17,9 @@ class AuthController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8',
-            'image' => 'required|image',
+            'image' => 'required|file|image',
             'phone' => 'required|string|max:255',
         ]);
-
-        try {
-            $image = $request->file('image')->store('public/images');
-        } catch (\Exception $e) {
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Invalid image file',
-                'data' => null
-            ], 400);
-        }
 
         if ($validator->fails()) {
             return response()->json([
@@ -37,6 +27,7 @@ class AuthController extends Controller
                 'message' => $validator->errors()
             ], 400);
         }
+        $image = $request->file('image')->store('public/images');
 
         $user = User::create([
             'name' => $request->name,
